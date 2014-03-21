56ece066df9e95ec9a223ffa10320d2dbded04c8
roofimon <roofimon@gmail.com>
2014-03-21 19:37:41 +0700

    2 basic test with ginkgo

---
+++ book_test.go
@@ -0,0 +1,26 @@
+package books_test
+
+import (
+  . "./model"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Book", func() {
+
+  var longBook Book = Book{Title: "Harry Potter", Author: "JK Rolling", Pages: 1000}
+  var shortBook Book = Book{Title: "Turn the ship around", Author: "Someone", Pages: 100}
+
+  Describe("Categorizing book length", func() {
+    Context("With more than 300 pages", func() {
+      It("should be a novel", func() {
+        Expect(longBook.CategoryByLength()).To(Equal("NOVEL"))
+      })
+    })
+    Context("With fewer than 300 pages", func() {
+      It("should ne an article", func() {
+        Expect(shortBook.CategoryByLength()).To(Equal("ARTICLE"))
+      })
+    })
+  })
+})
