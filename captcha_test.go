8d5ecedcec37bc5a4c87a61471207d81dfbf46e6
roofimon <roofimon@gmail.com>
2014-04-03 16:04:27 +0000

    Add many cases

--- captcha_test.go
+++ captcha_test.go
@@ -8,17 +8,40 @@ import (
 
 var _ = Describe("Captcha", func() {
   Context("First pattern with plus sign", func() {
-    It("should return One when input for first operand is 1", func() {
+    It("should return One when input for Left operand is 1", func() {
       var captcha = Captcha{Pattern: 1, LeftOperand: 1, Operator: 1, RightOperand: 1}
       Expect(captcha.GetLeftOperand()).To(Equal("One"))
     })
-    It("should return Two when input for first operand is 2", func() {
+    It("should return Two when input for Left operand is 2", func() {
       var captcha = Captcha{Pattern: 1, LeftOperand: 2, Operator: 1, RightOperand: 1}
       Expect(captcha.GetLeftOperand()).To(Equal("Two"))
     })    
-    It("should return Three when input for first operand is 3", func() {
+    It("should return Three when input for Left operand is 3", func() {
       var captcha = Captcha{Pattern: 1, LeftOperand: 3, Operator: 1, RightOperand: 1}
       Expect(captcha.GetLeftOperand()).To(Equal("Three"))
+    })
+    It("should return Four when input for Left operand is 4", func() {
+      var captcha = Captcha{Pattern: 1, LeftOperand: 4, Operator: 1, RightOperand: 1}
+      Expect(captcha.GetLeftOperand()).To(Equal("Four"))
+    })
+    
+    It("should return 1 when input for Right operand is 1", func() {
+      var captcha = Captcha{Pattern: 1, LeftOperand: 4, Operator: 1, RightOperand: 1}
+      Expect(captcha.GetRightOperand()).To(Equal("1"))
+    })
+    
+    It("should return 2 when input for Right operand is 2", func() {
+      var captcha = Captcha{Pattern: 1, LeftOperand: 4, Operator: 1, RightOperand: 2}
+      Expect(captcha.GetRightOperand()).To(Equal("2"))
+    })
+    
+    It("should return 3 when input for Plus with Left Operand is 1 and Right operand is 2", func() {
+      var captcha = Captcha{Pattern: 1, LeftOperand: 1, Operator: 1, RightOperand: 2}
+      Expect(captcha.GetResult()).To(Equal("3"))
+    })
+    It("should return 4 when input for Plus with Left Operand is 2 and Right operand is 2", func() {
+      var captcha = Captcha{Pattern: 1, LeftOperand: 2, Operator: 1, RightOperand: 2}
+      Expect(captcha.GetResult()).To(Equal("4"))
     })
   })
 })
--- model/captcha.go
+++ model/captcha.go
@@ -1,9 +1,18 @@
 package model 
+import "strconv"
 type Captcha struct {
   Pattern, LeftOperand, Operator, RightOperand int
 }
 
 func (captcha Captcha) GetLeftOperand() string {
-  var NumberInString = [3]string{"One", "Two", "Three"}
-  return NumberInString[captcha.LeftOperand-1]
+  var NumberInString map[int]string = map[int]string{ 1: "One", 2: "Two", 3: "Three", 4: "Four",5: "Five",6: "Six",7: "Seven",8: "Eight", 9: "Nine"}
+  return NumberInString[captcha.LeftOperand]
+}
+  
+func (captcha *Captcha) GetRightOperand() string {
+  return strconv.Itoa(captcha.RightOperand) 
+}
+  
+func (captcha *Captcha) GetResult() string {
+  return strconv.Itoa(captcha.LeftOperand + captcha.RightOperand)
 }
